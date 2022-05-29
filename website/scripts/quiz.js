0544c1f5fa3013102bd70085776af49a80135927
Marie-Alix GILLYBOEUF <marie-alix@MacBook-Pro-de-Marie-Alix.local>
2022-05-29 10:28:23 +0200

    quiz

--- website/scripts/quiz.js
+++ website/scripts/quiz.js
@@ -165,6 +165,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
             + '<input type="radio" name="question'+i+'" value="'+letter+'">'
             + letter + ': '
             + questions[i].answers[letter]
+            + '<br>'
           + '</label>'
         );
       }
